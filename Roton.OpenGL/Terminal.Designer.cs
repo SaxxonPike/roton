5b81b35ec1f24657315740602fe7e7d012dae9a2
Ian Burgmyer <spectere@gmail.com>
2015-01-29 08:39:06 -0500

    Added base Roton.OpenGL project.

---
+++ Roton.OpenGL/Terminal.Designer.cs
@@ -0,0 +1,38 @@
+﻿namespace Roton.OpenGL {
+    partial class Terminal {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.timerDaemon = new Roton.Windows.TimerDaemon(this.components);
+            this.SuspendLayout();
+            // 
+            // timerDaemon
+            // 
+            this.timerDaemon.Paused = false;
+            // 
+            // Terminal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Name = "Terminal";
+            this.Size = new System.Drawing.Size(640, 350);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Windows.TimerDaemon timerDaemon;
+    }
+}
---
+++ Roton.OpenGL/Terminal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using OpenTK.Graphics.OpenGL;
+using Roton;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace Roton.OpenGL {
+    public partial class Terminal : UserControl, ITerminal
+    {
+        private bool _canDraw = false;
+
+        public Terminal()
+        {
+            InitializeComponent();
+
+            // Initialize the GLControl late so that the WinForm designer
+            // doesn't crash.
+            var glControl = new GLControl {Dock = DockStyle.Fill};
+            glControl.Load += glControl_Load;
+        }
+
+        void glControl_Load(object sender, EventArgs e)
+        {
+            _canDraw = true;
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public void Plot(int x, int y, AnsiChar ac)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetSize(int width, int height, bool wide)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write(int x, int y, string value, int color)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
