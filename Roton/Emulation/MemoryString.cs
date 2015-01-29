576a2ae95f02b600003f13872bffe2e7485732f8
SaxxonPike <saxxonpike@gmail.com>
2015-01-29 08:55:21 -0600

    Add MemoryString

---
+++ Roton/Emulation/MemoryString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roton.Emulation
+{
+    sealed internal class MemoryString
+    {
+        public static implicit operator string(MemoryString source)
+        {
+            return source.ToString();
+        }
+
+        public MemoryString(Memory memory, int offset)
+        {
+            this.Memory = memory;
+            this.Offset = offset;
+        }
+
+        public Memory Memory
+        {
+            get;
+            private set;
+        }
+
+        public int Offset
+        {
+            get;
+            private set;
+        }
+
+        public override string ToString()
+        {
+            return Memory.ReadString(Offset);
+        }
+    }
+}
