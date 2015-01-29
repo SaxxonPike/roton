0e962ce22ff69eb96e0a4c7143e7f2bf2b61168e
SaxxonPike <saxxonpike@gmail.com>
2015-01-29 10:32:31 -0600

    Add CodeSearchInfo for label evaluation context

---
+++ Roton/Emulation/CodeSearchInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roton.Emulation
+{
+    sealed internal class CodeSearchInfo
+    {
+        public int Index;
+        public string Label;
+        public int Offset;
+
+        public CodeSearchInfo(string label)
+        {
+            this.Index = 0;
+            this.Label = label;
+            this.Offset = 0;
+        }
+    }
+}
