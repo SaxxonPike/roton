f762d9c4b6720cb65c346e771621097e4a6d4d9e
Tony Konzel <saxxonpike@gmail.com>
2018-07-20 20:42:52 -0500

    Make tests work in NetCoreApp2.

--- Roton.Test/Infrastructure/ContextBaseTestFixture.cs
+++ Roton.Test/Infrastructure/ContextBaseTestFixture.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Autofac;
 using AutoFixture;
 using AutoFixture.Dsl;
@@ -77,6 +78,8 @@ namespace Roton.Test.Infrastructure
         [SetUp]
         public void __SetUpContext()
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            
             // Test dependencies
             FileSystem = new FixedFileSystem(true);
             Config = new Config();
