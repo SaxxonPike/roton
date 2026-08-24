fb92539f02c362ac32124a4bd37cbdaa8b6404e7
Saxxon Fox <saxxonpike@gmail.com>
2026-08-24 00:10:17 -0500

    Convert to extension block

--- Source/Lyon/ServiceCollectionExtensions.cs
+++ Source/Lyon/ServiceCollectionExtensions.cs
@@ -18,49 +18,48 @@ public static class ServiceCollectionExtensions
 {
     private static readonly ThreadLocal<Stack<Type>> DependencyStack = new(() => []);
     
-    public static void AddLyon(
-        this IServiceCollection services,
-        string[] args,
-        IConfig config)
+    extension(IServiceCollection services)
     {
-        services.AddSingleton(config);
-        services.AddSingleton<ICommandLine>(new CommandLine { Args = args });
-
-        services.AddSingleton<IFileSystem>(c =>
-            c.GetRequiredService<IFileSystemFactory>().Create(c.GetRequiredService<IConfig>().HomePath ?? ".")
-        );
-
-        services.AddSingleton<ISceneComposer>(c => c.GetRequiredService<ISceneComposerFactory>().Get());
-        services.AddSingleton<ISpeaker>(c => c.GetRequiredService<IAudioComposer>());
-        services.AddSingleton<ITerminal>(c => c.GetRequiredService<ISceneComposer>());
-    }
-
-    public static void AddRoton(
-        this IServiceCollection services,
-        Context context,
-        params Assembly[] additionalAssemblies)
-    {
-        var map = RotonServices.Get(context, additionalAssemblies)
-            .GroupBy(s => s.Implementation);
-
-        foreach (var serviceGroup in map)
+        public void AddLyon(string[] args,
+            IConfig config)
         {
-            // Add concrete implementation.
-            services.AddSingleton(serviceGroup.Key);
+            services.AddSingleton(config);
+            services.AddSingleton<ICommandLine>(new CommandLine { Args = args });
 
-            // Add service mappings.
-            foreach (var service in serviceGroup)
-                services.AddSingleton(service.Service, sp =>
-                {
-                    var stack = DependencyStack.Value!;
-                    if (stack.Contains(service.Service))
-                        throw new Exception($"Circular dependency detected: {service.Service.FullName} -> " +
-                                            string.Join(" -> ", stack.Select(rs => rs.ToString())));
-                    stack.Push(service.Service);
-                    var result = sp.GetRequiredService(serviceGroup.Key);
-                    stack.Pop();
-                    return result;
-                });
+            services.AddSingleton<IFileSystem>(c =>
+                c.GetRequiredService<IFileSystemFactory>().Create(c.GetRequiredService<IConfig>().HomePath ?? ".")
+            );
+
+            services.AddSingleton<ISceneComposer>(c => c.GetRequiredService<ISceneComposerFactory>().Get());
+            services.AddSingleton<ISpeaker>(c => c.GetRequiredService<IAudioComposer>());
+            services.AddSingleton<ITerminal>(c => c.GetRequiredService<ISceneComposer>());
+        }
+
+        public void AddRoton(Context context,
+            params Assembly[] additionalAssemblies)
+        {
+            var map = RotonServices.Get(context, additionalAssemblies)
+                .GroupBy(s => s.Implementation);
+
+            foreach (var serviceGroup in map)
+            {
+                // Add concrete implementation.
+                services.AddSingleton(serviceGroup.Key);
+
+                // Add service mappings.
+                foreach (var service in serviceGroup)
+                    services.AddSingleton(service.Service, sp =>
+                    {
+                        var stack = DependencyStack.Value!;
+                        if (stack.Contains(service.Service))
+                            throw new Exception($"Circular dependency detected: {service.Service.FullName} -> " +
+                                                string.Join(" -> ", stack.Select(rs => rs.ToString())));
+                        stack.Push(service.Service);
+                        var result = sp.GetRequiredService(serviceGroup.Key);
+                        stack.Pop();
+                        return result;
+                    });
+            }
         }
     }
 }
