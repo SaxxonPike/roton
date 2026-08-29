d635c6112741f0d9fbb8182c873e791d536c626f
Saxxon Fox <saxxonpike@gmail.com>
2026-08-28 20:03:28 -0500

    Roll back trace WIP

--- Source/Roton/Emulation/Traces/ITraceList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using Roton.Emulation.Data;
-
-namespace Roton.Emulation.Traces;
-
-public interface ITraceList
-{
-    void Trace(TraceFlags flag, IEnumerable<KeyValuePair<string, object?>> data);
-}
