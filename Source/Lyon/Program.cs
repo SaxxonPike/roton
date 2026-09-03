5be47b0f3596232e3604ad9173462fd823c5119d
Saxxon Fox <saxxonpike@gmail.com>
2026-09-03 12:05:24 -0500

    Parse command line arguments a little better (getopts style)

--- Source/Lyon/Program.cs
+++ Source/Lyon/Program.cs
@@ -11,16 +11,28 @@ using Roton.Emulation.Core;
 using Roton.Emulation.Data.Impl;
 using Roton.Infrastructure.Impl;
 
-// Process command line arguments.
-var fileName = args.TakeWhile(s => !s.StartsWith("--")).FirstOrDefault();
-var switches = args.SkipWhile(s => !s.StartsWith("--")).Select(s => s.ToLower()).ToArray();
+// Process command line arguments. This (loosely) follows the "GetOpt" convention.
+
+var switches = args
+    .TakeWhile(s => s != "--")
+    .Where(s => s.StartsWith("--") || s is ['-', _])
+    .ToList();
+
+var fileNames = args
+    .TakeWhile(s => s != "--")
+    .Where(s => !s.StartsWith("--") && s is not ['-', _])
+    .Concat(args.SkipWhile(s => s != "--"))
+    .DefaultIfEmpty("TOWN.ZZT")
+    .ToList();
+
+var fileName = fileNames.First();
 
 // Process configuration.
 var config = new Config
 {
     DefaultWorld = Path.GetFileNameWithoutExtension(fileName),
     RandomSeed = null,
-    HomePath = fileName != null ? Path.GetDirectoryName(fileName) : Environment.CurrentDirectory,
+    HomePath = Path.GetDirectoryName(fileName),
     AudioDrumRate = 64,
     AudioSampleRate = 44100,
     AudioBufferSize = 2048,
@@ -28,15 +40,13 @@ var config = new Config
     VideoScaleY = 2,
     MasterClockNumerator = 100,
     MasterClockDenominator = 7275,
-    FastMode = switches.Contains("--fast"),
-    TraceOop = switches.Contains("--trace"),
-    NoPesterMode = switches.Contains("--no-pester"),
+    FastMode = switches.Contains("--fast") || switches.Contains("-f"),
+    TraceOop = switches.Contains("--trace") || switches.Contains("-t"),
+    NoPesterMode = switches.Contains("--no-pester") || switches.Contains("-p"),
     JoystickDeadZone = 0.5f,
     JoystickDenoiseZone = 0.1f,
     DisableJoystick = switches.Contains("--no-joystick")
 };
-
-fileName ??= "TOWN.ZZT";
 
 // Determine which engine to use based on the world file name extension.
 if (!ContextSelector.TryGetForWorldFileName(fileName, out var contextEngine))
