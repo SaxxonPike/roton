9cb686b485da82033f2917d1976b794c19edaf69
Saxxon Fox <saxxonpike@gmail.com>
2026-09-07 01:10:00 -0500

    More RoZ remark docs

--- Source/Roton/Emulation/Core/IColorMatcher.cs
+++ Source/Roton/Emulation/Core/IColorMatcher.cs
@@ -2,5 +2,8 @@ namespace Roton.Emulation.Core;
 
 public interface IColorMatcher
 {
+    /// <remarks>
+    /// RoZ: GetColorForTileMatch
+    /// </remarks>
     int GetColorMatchValue(int color);
 }
